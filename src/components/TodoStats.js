e756f0118999a7a4e563b55dee586c624fe77733
Keshav Narula <narulakeshav13@gmail.com>
2017-06-05 16:19:20 -0700

    Remove console.log

--- src/components/TodoStats.js
+++ src/components/TodoStats.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../App.css';
 
 const TodoStats = (props) => {
-  console.log(props.percent);
   return (
     <div className="TodoStats">
       <p className="stats"><span>{props.list.length}</span> Tasks</p>
