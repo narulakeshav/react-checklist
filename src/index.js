c5d0d932bf990309491d007bd836d44b7e167a12
Keshav Narula <narulakeshav13@gmail.com>
2017-06-05 16:26:18 -0700

    Create method that gets data from localstorage

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-let todos = JSON.parse(localStorage.getItem('todos'));
-let finished = JSON.parse(localStorage.getItem('finished'));
-let done = JSON.parse(localStorage.getItem('done'));
-ReactDOM.render(<App
-                    todos={todos}
-                    finished={finished}
-                    done={done}/>, document.getElementById('app'));
+// Gets Item from localStorage
+let getItem = (val) => {
+  return JSON.parse(localStorage.getItem(val));
+};
+
+ReactDOM.render(<App todos={getItem('todos')} finished={getItem('finished')}
+                    done={getItem('done')}/>, document.getElementById('app'));
