a7734f4b66e8cedcc4c631a89b9ba016e7fe536b
Keshav Narula <narulakeshav13@gmail.com>
2017-06-05 16:31:36 -0700

    Turn ItemOptions to Stateless Functional Component (SFC)

--- src/components/ItemOptions.js
+++ src/components/ItemOptions.js
@@ -1,32 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import '../App.css';
 
-class ItemOptions extends Component {
-    render() {
-        return (
-            <div className="TaskOptions">
-                <button onClick={this._removeItem.bind(this)}>
-                    <span className="fa fa-trash"></span>
-                </button>
-            </div>
-        );
-    }
-
-    _removeItem(e) {
-        let todoList = this.props.todoList,
+const ItemOptions = (props) => {
+    const _removeItem = (e) => {
+        let todoList = props.todoList,
             i = 0,
             found = false;
         // Remove this todo from the array
         while (i < todoList.length && !found) {
-            if (this.props.item === todoList[i]) {
+            if (props.item === todoList[i]) {
               todoList.splice(i, 1); // remove that item from array
               found = true;
             }
             i++;
         }
         // Then call the update local storage method
-        this.props.updateList(todoList);
-    }
-}
+        props.updateList(todoList);
+    };
+
+    // Return
+    return (
+        <div className="TaskOptions">
+            <button onClick={_removeItem}>
+                <span className="fa fa-trash"></span>
+            </button>
+        </div>
+    );
+};
 
 export default ItemOptions;
