63d9b7904cc9897636450f2c0d606502485f7356
Keshav Narula <narulakeshav13@gmail.com>
2017-06-05 16:16:32 -0700

    Grab data from localStorage from Render

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import './index.css';
 
 let todos = JSON.parse(localStorage.getItem('todos'));
 let finished = JSON.parse(localStorage.getItem('finished'));
-ReactDOM.render(<App todos={todos} finished={finished}/>, document.getElementById('app'));
+let done = JSON.parse(localStorage.getItem('done'));
+ReactDOM.render(<App
+                    todos={todos}
+                    finished={finished}
+                    done={done}/>, document.getElementById('app'));
