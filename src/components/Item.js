4d25a5497b9b4f92e65cffbb7ecfc1c2c41af6e2
Keshav Narula <narulakeshav13@gmail.com>
2017-06-05 16:34:44 -0700

    Turn Item into SFC as well -- let's speed it up

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,30 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ItemOptions from './ItemOptions';
 import '../App.css';
 
-class Item extends Component {
-    render() {
-        let name = this.props.item.task,
-            completed = this.props.item.completed,
-            listClass = (completed) ? 'done' : '';
-        return (
-            <li className={listClass}>
-                <div className="ItemDiv" onClick={this._markDone.bind(this)}>
-                    <p>{name}</p>
-                </div>
-                <ItemOptions
-                    item={this.props.item}
-                    todoList={this.props.list}
-                    updateList={this.props.updateList}/>
-            </li>
-        );
-    }
+const Item = (props) => {
+    // Marks x task as done
+    const _markDone = () => {
+        let currentTask = props.item;
+        props.completeTask(currentTask);
+    };
 
-    _markDone() {
-        // Update States
-        let currentTask = this.props.item;
-        this.props.completeTask(currentTask);
-    }
-}
+    // Cache Values
+    let name = props.item.task,
+        completed = props.item.completed,
+        listClass = (completed) ? 'done' : '';
+
+    // return
+    return (
+        <li className={listClass}>
+            <div className="ItemDiv" onClick={_markDone}>
+                <p>{name}</p>
+            </div>
+            <ItemOptions
+                item={props.item}
+                todoList={props.list}
+                updateList={props.updateList}/>
+        </li>
+    );
+};
 
 export default Item;
